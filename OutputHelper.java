b7ee94b17bf3f3b0c01e1e34531bcb3b9fce3df8
id27344 <chrisgenga10@gmail.com>
2025-11-22 07:15:34 +0200

    Add files via upload
    This file contains custom method that will be used to display the program's output with 
    concatenation of the student id.

---
+++ OutputHelper.java
@@ -0,0 +1,11 @@
+package id_27344.utils;
+
+public class OutputHelper {
+
+    // Reusable method to print any message with student ID
+    public static void printWithID(String message) {
+        System.out.println(message + " - 27344");
+    }
+
+}
+
